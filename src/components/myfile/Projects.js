6830fba9ac46a2188ed107efeee7ac38de21beff
junhyeon0218 <junhyeon0218@daum.net>
2024-01-06 19:18:52 +0900

    Feat: 프로젝트 작성

--- src/components/myfile/Projects.js
+++ src/components/myfile/Projects.js
@@ -4,7 +4,55 @@ import classes from "../../styles/myfile/Projects.module.css";
 const Projects = () => {
   return (
     <Fragment>
-      <div>프로젝트</div>
+      <div className={classes.container}>
+        <div className={classes.mainTitle}>💻 프로젝트</div>
+        <div className={classes.projectList}>
+          <div className={classes.project}>
+            <div className={classes.coverImg}>사진</div>
+            <div className={classes.projectDetail}>
+              <div className={classes.projectTitle}>프로젝트 제목</div>
+              <div className={classes.projectTimeLine}>2022/02 - 2022/10</div>
+              <div className={classes.projectInfo}>설명</div>
+            </div>
+          </div>
+
+          <div className={classes.project}>
+            <div className={classes.coverImg}>사진</div>
+            <div className={classes.projectDetail}>
+              <div className={classes.projectTitle}>프로젝트 제목</div>
+              <div className={classes.projectTimeLine}>2022/02 - 2022/10</div>
+              <div className={classes.projectInfo}>설명</div>
+            </div>
+          </div>
+
+          <div className={classes.project}>
+            <div className={classes.coverImg}>사진</div>
+            <div className={classes.projectDetail}>
+              <div className={classes.projectTitle}>프로젝트 제목</div>
+              <div className={classes.projectTimeLine}>2022/02 - 2022/10</div>
+              <div className={classes.projectInfo}>설명</div>
+            </div>
+          </div>
+
+          <div className={classes.project}>
+            <div className={classes.coverImg}>사진</div>
+            <div className={classes.projectDetail}>
+              <div className={classes.projectTitle}>프로젝트 제목</div>
+              <div className={classes.projectTimeLine}>2022/02 - 2022/10</div>
+              <div className={classes.projectInfo}>설명</div>
+            </div>
+          </div>
+
+          <div className={classes.project}>
+            <div className={classes.coverImg}>사진</div>
+            <div className={classes.projectDetail}>
+              <div className={classes.projectTitle}>프로젝트 제목</div>
+              <div className={classes.projectTimeLine}>2022/02 - 2022/10</div>
+              <div className={classes.projectInfo}>설명</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
